7b6782a382df1d3de643a20c0e19ee3448a0d96f
Opilane TTHK <opilane@tthk.ee>
2022-01-05 16:05:51 +0200

    SnakeOOP

---
+++ SnakeOOP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SnakeOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(10, 10, '*');
+            Point p2 = new Point(11, 10, '*');
+
+            HorizontalLine hLine = new HorizontalLine(10, 15, 5, '*');
+            hLine.Draw();
+            VerticalLine vLine = new VerticalLine(6, 16, 10, '@');
+
+            HorizontalLine top = new HorizontalLine(0, 80, 0, '#');
+            top.Draw();
+            VerticalLine left = new VerticalLine(0, 25, 0, '@');
+            left.Draw();
+            HorizontalLine bottom = new HorizontalLine(0, 80, 25, '$');
+            bottom.Draw();
+            VerticalLine right = new VerticalLine(0, 25, 80, '$');
+            right.Draw();
+
+            Point snakeTail = new Point(15, 15, 's');
+            Snake snake = new Snake(snakeTail, 10, Direction.DOWN);
+            snake.Draw();
+
+            Console.ReadLine();
+        }
+
+        
+
+    }
+}
